968efa97662db86652fcb4c4feeedceb1af9ca14
Adi Kautsar <agiljibrin009@gmail.com>
2024-09-03 12:03:21 +0800

    feat: menyelesaikan migrasi table

--- website-cbt/database/migrations/2024_09_02_121143_create_course_answers_table.php
+++ website-cbt/database/migrations/2024_09_02_121143_create_course_answers_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('course_answers', function (Blueprint $table) {
             $table->id();
+            $table->string('answer');
+            $table->boolean('is_correct');
+            $table->foreignId('course_question_id')->constrained()->onDelete('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
